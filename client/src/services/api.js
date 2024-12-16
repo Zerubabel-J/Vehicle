53b73348dd4b43b71ba4fb7b641ccc18b7c05613
Zerubabel <zerucodes@gmail.com>
2024-12-16 16:38:50 +0300

    service api added

---
+++ client/src/services/api.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:5000/api";
+
+export const fetchVehicles = async () => {
+  const response = await axios.get(`${API_URL}/vehicles`);
+  return response.data;
+};
+
+export const addVehicle = async (vehicle) => {
+  const response = await axios.post(`${API_URL}/vehicles`, vehicle);
+  return response.data;
+};
+
+export const updateVehicle = async (id, updates) => {
+  const response = await axios.put(`${API_URL}/vehicles/${id}`, updates);
+  return response.data;
+};
+
+export const deleteVehicle = async (id) => {
+  const response = await axios.delete(`${API_URL}/vehicles/${id}`);
+  return response.data;
+};
+
+export const fetchStatistics = async () => {
+  const response = await axios.get(`${API_URL}/statistics`);
+  return response.data;
+};
